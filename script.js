edf56f2ec36ab3d2878e75c8137e2d846b181dbb
Reutskov-Dmitriy <infowelc@gmail.com>
2022-08-06 19:35:23 +0300

    change getElementById

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function calculateAreaCircle(){
-	const PI = 3.14;
+
+
+function calculateAreaCircle() {
 	let radiusCircle;
-	radiusCircle = document.getElementById("area").value;
-	// alert(`Area of a circle = ${PI * radiusCircle**2}`);
-	document.getElementById("area").innerHTML = `Area of a circle = ${PI * radiusCircle**2}`;
+	radiusCircle = document.getElementById("radius").value;
+	document.getElementById("area").innerHTML = `Area of a circle = ${Math.round(Math.PI * (+radiusCircle) ** 2)}`;
 
 }
